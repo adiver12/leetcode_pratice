eeae9a7d6f3cb929e0c922ededb25b6c604b9624
adiver12 <109779311+adiver12@users.noreply.github.com>
2022-08-17 23:18:10 +0530

    Time: 219 ms (5.06%), Space: 55.3 MB (66.91%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        
-    int window_sum = 0;
-        int count = 0;
-
-        // step 1 : forming window sum for first window [starting at index (0) and ending at index (k - 1)]
-        for(int i = 0 ; i < k ; ++i)
-            window_sum += arr[i];
-        
-        // step 2 : loop through arr and find each window's average
-        for(int i = 0 ; i < arr.size() - k + 1 ; ++i)
-        {
-            if(i != 0)
-            {
-                window_sum -= arr[i - 1];    // removing first element from prev window
-                window_sum += arr[i + k - 1];// adding last + 1 elemnt in prev window
-                // new window is ready
-            }
-            
-            int window_average = window_sum / k;
-            
-            if(window_average >= threshold)
-                count++;
+    int numOfSubarrays(vector<int>& arr, int k, int th) {
+        int n =arr.size();
+        int rsum=0;
+        for(int i=0;i<k;++i) rsum+=arr[i];
+        int ans=0;
+        if(rsum>=th*k)++ans;
+        int low=0;
+        for(int high=k;high<n;++high){
+            rsum-=arr[high-k];
+            rsum+=arr[high];
+            // ++low;
+            if(rsum>=th*k)++ans;
         }
-        
-        return count;
-        
+        return ans;
     }
 };
