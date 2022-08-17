d121f46efe72e2377792b98af973bb59db4e24cb
adiver12 <109779311+adiver12@users.noreply.github.com>
2022-08-17 21:30:43 +0530

    Time: 52 ms (29.46%), Space: 10.1 MB (67.86%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -3,13 +3,16 @@ public:
     int maxConsecutiveAnswers(string answerKey, int k) {
          int res = 0, maxf = 0;
         unordered_map<int, int> count;
-        for (int i = 0; i < answerKey.length(); ++i) {
+        
+        for (int i = 0; i < answerKey.length(); ++i)
+        {
             maxf = max(maxf, ++count[answerKey[i]]);
             if (res - maxf < k)
                 res++;
             else
                 count[answerKey[i - res]]--;
         }
+        
         return res;
     }
 };
