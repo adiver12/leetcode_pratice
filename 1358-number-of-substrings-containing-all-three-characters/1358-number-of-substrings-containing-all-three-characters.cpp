b6d4fa24f001b6c3124dab8229a150233ef44ac6
adiver12 <109779311+adiver12@users.noreply.github.com>
2022-08-17 19:31:35 +0530

    Time: 23 ms (86.45%), Space: 8.6 MB (56.38%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         
-     int count[3] = {0, 0, 0},ans = 0 , i = 0;
+     int count[3] = {0, 0, 0},ans = 0 ;
+        int i = 0;  //left pointer
+        // int j=0; // right pointer
         
         for (int j = 0; j < s.length(); ++j) 
         {
             ++count[s[j] - 'a'];
             
            while (count[0] && count[1] && count[2] && i<=j){
+               
                 --count[s[i] - 'a'];
-               ans+=(s.length()-j);
+               ans+=(s.length()-j);  // agar abc agaye hai toh baad mein jitney bhi letter hai sab answer hoga jaise phele abc aagaye hai 
                i++;
             }
             
