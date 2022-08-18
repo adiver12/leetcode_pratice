3c6c67b294d095ca18a973a15de6afddbb0273de
adiver12 <109779311+adiver12@users.noreply.github.com>
2022-08-18 10:19:46 +0530

    Time: 45 ms (9.29%), Space: 8.5 MB (68.53%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    int maxSumTwoNoOverlap(vector<int>& nums, int fln, int sln) {
+    int maxSumTwoNoOverlap(vector<int>& v, int fln, int sln) {
         int res=INT_MIN;
-        for(int i=0;i<=nums.size()-fln-sln;++i){
+        for(int i=0;i<v.size()-fln-sln+1;++i){
             int sum1=0,sum2=0;
             for(int j=i;j<i+fln;++j){
-                sum1+=nums[j];
+                sum1+=v[j];
             }
-            for(int j=i+fln;j<=nums.size()-sln;++j){
-                sum2=0;
-                for(int k=j;k<j+sln;++k){
-                    sum2+=nums[k];
-                }
+            for(int j=i+fln;j<i+fln+sln;++j){
+                sum2+=v[j];
+            }
+            res=max(res,sum1+sum2);
+            int low=i+fln,high=i+fln+sln;
+            while(high<v.size()){
+                sum2-=v[low];
+                ++low;
+                sum2+=v[high];
+                ++high;
                 res=max(res,sum1+sum2);
             }
         }
         swap(fln,sln);
-        for(int i=0;i<=nums.size()-fln-sln;++i){
+
+        for(int i=0;i<v.size()-fln-sln+1;++i){
             int sum1=0,sum2=0;
             for(int j=i;j<i+fln;++j){
-                sum1+=nums[j];
+                sum1+=v[j];
             }
-            for(int j=i+fln;j<=nums.size()-sln;++j){
-                sum2=0;
-                for(int k=j;k<j+sln;++k){
-                    sum2+=nums[k];
-                }
+            for(int j=i+fln;j<i+fln+sln;++j){
+                sum2+=v[j];
+            }
+            res=max(res,sum1+sum2);
+            int low=i+fln,high=i+fln+sln;
+            while(high<v.size()){
+                sum2-=v[low];
+                ++low;
+                sum2+=v[high];
+                ++high;
                 res=max(res,sum1+sum2);
             }
-        }        
+        }
+        
         return res;
     }
 };
