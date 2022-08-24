2cd76ac8a9dcc3048328c1e1a118f869fd0f6ddc
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-08-24 09:54:44 +0530

    Time: 3 ms (99.15%), Space: 7.7 MB (51.71%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
         int start=0,sum1=0,count=0;
-        int end=0; 
-        for( ;end<s.length();end++){
+          
+        for(int end=0;end<s.length();end++){
             sum1+=abs(s[end]-t[end]);
             count++;
             if(sum1>maxCost){
