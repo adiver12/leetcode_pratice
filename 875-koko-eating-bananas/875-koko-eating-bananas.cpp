714b807a889de4e663f399a3d2e001f0c2fba407
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-08-25 14:39:24 +0530

    Time: 65 ms (67.56%), Space: 19 MB (8.90%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -7,15 +7,15 @@ public:
         
         while (l < r)
         {
-            int m = (l + r) / 2, total = 0;
+            int m = (l + r) / 2, hours = 0;
  // calculate val for each mid assumed
 // this loops tell the time taken to eat all bananas by the current mid
             for (int p : piles)
-                total += (p + m - 1) / m;
+                hours += ceil(1.0 * p  / m);
   // if the time is less than the value given to us we reduce the high as we need 
 // to increase time by decreasing high we increase time
             
-            if (total > H)
+            if (hours > H)
                 l = m + 1;
             else
                 r = m;
