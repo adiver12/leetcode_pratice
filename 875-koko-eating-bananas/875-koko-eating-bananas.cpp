fbeea5e064a14a2b8cb2085d10c1175407d56bf0
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-08-25 14:47:30 +0530

    Time: 48 ms (91.37%), Space: 19 MB (8.90%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -3,22 +3,22 @@ public:
     int minEatingSpeed(vector<int>& piles, int H) {
           int l = 1, r = 1000000000;
 //      video by neet code 
-        
+//       if 1 banana is their minimum speed will be 1 and if 10^9 banana in 1 day will be their speed will be that.  
         
         while (l < r)
         {
-            int m = (l + r) / 2, hours = 0;
+            int speed = (l + r) / 2, hours = 0;
  // calculate val for each mid assumed
 // this loops tell the time taken to eat all bananas by the current mid
             for (int p : piles)
-                hours += ceil(1.0 * p  / m);
+                hours += ceil(1.0*p/speed);
   // if the time is less than the value given to us we reduce the high as we need 
 // to increase time by decreasing high we increase time
             
             if (hours > H)
-                l = m + 1;
+                l = speed + 1;
             else
-                r = m;
+                r = speed;
         }
         return l;
         
