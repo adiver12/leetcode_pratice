7020cb8498b93b81e3e6a81fad8c95b8797a7fdb
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-01 09:34:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.64%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         
-   stack<int> st;
+       stack<int> st;
         int score = 0;
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '('){
