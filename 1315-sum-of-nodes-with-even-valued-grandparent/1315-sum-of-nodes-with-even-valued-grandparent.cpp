cbe4c997b285b6e7180f02ba75d44ca6ced77e2d
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-02 11:07:32 +0530

    Time: 91 ms (26.96%), Space: 41.5 MB (43.46%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -21,14 +21,19 @@ public:
 		//check node with even-valued grandparent add grandchildren value in sum if exist
 		
         if(root->val%2==0){
-            if(root->left!=NULL && root->left->left!=NULL)
-                sum+=root->left->left->val;
-            if(root->left!=NULL && root->left->right!=NULL)
-                sum+=root->left->right->val;
-            if(root->right!=NULL && root->right->right!=NULL)
-                sum+=root->right->right->val;
-            if(root->right!=NULL && root->right->left!=NULL)
-                sum+=root->right->left->val;
+            if(root->left!=NULL){
+                if(root->left->left!=NULL)
+                    sum+=root->left->left->val;
+                if(root->left->right!=NULL)
+                    sum+=root->left->right->val;                
+            }
+ 
+            if(root->right!=NULL){ 
+                if(root->right->right!=NULL)
+                   sum+=root->right->right->val;
+                if(root->right->left!=NULL)
+                   sum+=root->right->left->val;
+        }
         }
 		
 		//dfs
