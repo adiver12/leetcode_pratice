4f617b5870c4b983c9249167563b3fac664a8bac
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-15 14:22:34 +0530

    Time: 10 ms (31.94%), Space: 10.5 MB (96.32%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,11 +11,13 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-      
+//    When you move the fast pointer to nth node, the remaining nodes to traverse is (size_of_linked_list - n). After that, when you start moving slow pointer and fast pointer by 1 node each, it is guaranteed that slow pointer will cover a distance of (size_of_linked_list - n) nodes. And that's node we want to remove.   
         ListNode *fast = head, *slow = head;
         for (int i = 0; i < n; i++) {
             fast = fast->next;
         }
+//         this is edge case when n=5 in the first case then this will return ( head->next will return the desired result)
+        
         if (fast == NULL)  
             return head->next;
         
