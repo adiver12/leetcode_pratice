f08af8ad77f3fdf7aaab36de9f7ee5c083141e7b
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-16 16:06:12 +0530

    Time: 2014 ms (22.07%), Space: 294.1 MB (99.44%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,17 +12,18 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
 //  We can solve this problem with two pointer approach: idea is to make two pointers - fast pointer(jumps two node) and slow pointer(jumps one node). And when fast pointer reaches end, slow pointer would have reached middle element.
+       if(head->next==NULL){
+           return NULL;
+       } 
+      ListNode* slow=head;
+      ListNode* fast=head->next->next;
         
-    if (head->next == NULL)
-        return NULL;
-        
-    auto slow = head, fast = head->next->next;
-        
-    while (fast != NULL  && fast->next != NULL) {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    slow->next = slow->next->next;
-    return head;
+      while(fast != NULL  and fast->next != NULL){
+          slow=slow->next;
+          fast=fast->next->next;
+      }  
+        slow->next=slow->next->next;
+        return head;
+         
     }
 };
