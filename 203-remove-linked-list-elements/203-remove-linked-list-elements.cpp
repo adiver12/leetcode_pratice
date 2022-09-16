57293ccad901321e21d0c6cb26fbaf56542204f1
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-16 16:16:01 +0530

    Time: 58 ms (8.91%), Space: 15 MB (46.76%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,29 +11,24 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if(head==NULL){
-            return NULL;
+   if(head==nullptr) return head;
+		// This is for the case when a linked list is like this: 
+		// 1->1->2->null , val = 1
+		// 1->1->1->null , val = 1
+        while(head!=nullptr && head->val==val){
+            head = head->next;
         }
-        
-        while(head!=NULL  and head->val==val){
-            head=head->next;
-        }
-        ListNode* temp=head;
-        ListNode* slow=NULL;
-        
-        
-        while(temp != NULL){
-            
-            if(temp->val==val){
-                slow->next=slow->next->next;
-                 temp=head;
+        ListNode* curr = head;
+        while(curr!=nullptr  && curr->next!=nullptr){
+            if(curr->next->val==val){
+                curr->next = curr->next->next;
+				// After doing the above step, I am not updating "curr" because of these type of test cases:
+				// 1->2->3->6->6->6->5->null  val = 6
             }
-            else{
-                slow=temp;
-                temp=temp->next;   
-            }
+            else
+                curr = curr->next;
         }
-        
-        return head; 
+        return head;
+
     }
 };
