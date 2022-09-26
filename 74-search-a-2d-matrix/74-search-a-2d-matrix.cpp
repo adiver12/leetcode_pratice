6606d05186a7397045a8d8dde21700e4fce3b0ac
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-26 11:07:34 +0530

    Time: 13 ms (13.37%), Space: 9.6 MB (5.11%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-     
-     int n=matrix.size();
-     int m=matrix[0].size();
+		// for(int i  = 0; i<matrix.size(); ++i)
+		// {
+		// // nested for for ietrating each row element
+		// for(int j = 0; j<matrix[0].size(); ++j)
+		// {
+		// 		// if found return true
+		// if(matrix[i][j] == target)
+		// return true;
+		// }
+		// }
+		// // after traversal if not found
+		// // return false
+		// return false;  
         
+        int n = matrix.size();
+        int m = matrix[0].size();
         
-      int start=0,end=n*m-1;
+		// if matrix have 0 rows or 0 colums
+        if(n == 0 || m == 0)
+            return false;
         
-        while(start<=end){
-            int mid=start+(end-start)/2 ;
-            
-            if(matrix[mid/m][mid%m]==target){
+		// treating matrix as array just taking care of endices
+		// [0..n*m]
+        int start = 0, end = m*n - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+			// a[x] : matrix[x / m][x % m] formulae
+              
+            if (target == matrix[mid/m][mid%m])
                 return true;
-            }
-            else if(matrix[mid/m][mid%m]>target){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-     }
+			// left half
+            else if(target < matrix[mid/m][mid%m])
+                end = mid - 1;
+            else
+			// right half
+                start = mid + 1;       
+        }
         return false;
-  }
+    }
 };
