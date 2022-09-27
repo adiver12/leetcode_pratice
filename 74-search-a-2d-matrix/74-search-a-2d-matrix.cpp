720350903ea36cf28153cdcb2417a411a3064403
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-27 20:23:04 +0530

    Time: 10 ms (32.09%), Space: 9.3 MB (95.03%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-		// for(int i  = 0; i<matrix.size(); ++i)
-		// {
-		// // nested for for ietrating each row element
-		// for(int j = 0; j<matrix[0].size(); ++j)
-		// {
-		// 		// if found return true
-		// if(matrix[i][j] == target)
-		// return true;
-		// }
-		// }
-		// // after traversal if not found
-		// // return false
-		// return false;  
+     
+       int n=matrix.size();
+       int m=matrix[0].size();
         
-        int n = matrix.size();
-        int m = matrix[0].size();
+       int start=0,end=n*m-1;
         
-		// if matrix have 0 rows or 0 colums
-        if(n == 0 || m == 0)
-            return false;
-        
-		// treating matrix as array just taking care of endices
-		// [0..n*m]
-        int start = 0, end = m*n - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-			// a[x] : matrix[x / m][x % m] formulae
-              
-            if (target == matrix[mid/m][mid%m])
-                return true;
-			// left half
-            else if(target < matrix[mid/m][mid%m])
-                end = mid - 1;
-            else
-			// right half
-                start = mid + 1;       
-        }
+       while(start<= end){
+           int mid=start+(end-start)/2;
+           
+           if(matrix[mid/m][mid%m]==target){
+               return true;
+           }
+           else if(matrix[mid/m][mid%m] > target){
+               end=mid-1;
+           }
+           else if(matrix[mid/m][mid%m] < target){
+               start=mid+1;
+           }
+       }
         return false;
-    }
+  }
 };
