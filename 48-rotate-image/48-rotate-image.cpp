d2b55176390338663495c18ea20949b55cb741ea
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-09-27 20:28:27 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.38%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -6,8 +6,8 @@ public:
         
         
  	// complement of matrix 
-    
-       int n=matrix.size();
+       
+        int n=matrix.size();
         
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
@@ -16,9 +16,10 @@ public:
         }
         
         for(int i=0;i<n;i++){
-         int left=0,right=n-1;
+            int left=0,right=n-1;
             
-            while(left<right){
+            while(left<right) {
+               
                 swap(matrix[i][left],matrix[i][right]);
                 left++;
                 right--;
