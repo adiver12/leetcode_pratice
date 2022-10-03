d8c82fac9c13d80dbf7928dc0776c99377b9852e
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-03 16:02:53 +0530

    Time: 32 ms (10.25%), Space: 9.4 MB (29.88%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -12,20 +12,25 @@ public:
 // Time O(MNlogD), where D is the length of diagonal with D = min(M,N).
 // Space O(MN) 
         
+ // https://leetcode.com/problems/sort-the-matrix-diagonally/discuss/2493662/PythonororC%2B%2BEasiest-approachDetailed-Explanation-or-Beginner-friendly-or-Easy-understand
+    
+        int m=mat.size(),n=mat[0].size();
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>>map;
+            
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                map[i-j].push(mat[i][j]);
+            }
+        }
+ 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]=map[i-j].top();
+                map[i-j].pop();
+            }
+        }
         
-      int m = mat.size(), n = mat[0].size();
-        // all elements on same diagonal have same i-j result.
-        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map; // min priority queue
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i - j].push(mat[i][j]);
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = map[i - j].top(); map[i - j].pop();
-            }
-        }
         return mat;
+   
     }
 };
