d96bdda3f85d9114d4b20b3173d9182e98377019
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-07 14:46:24 +0530

    Time: 5 ms (13.98%), Space: 5.8 MB (83.21%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -16,16 +16,32 @@ public:
         
 //     }
     
-    int climbStairs(int n) {
-//    Memoization
-        unordered_map<int,int>dp;
-        dp[0]=1;
-        dp[1]=1;
+//     int climbStairs(int n) {
+// //    Tabulation
+//         unordered_map<int,int>dp;
+//         dp[0]=1;
+//         dp[1]=1;
        
-        for(int i=2;i<=n;i++) {
-           dp[i]=dp[i-1]+dp[i-2];
-         }
-         return dp[n];
-        }
+//         for(int i=2;i<=n;i++) {
+//            dp[i]=dp[i-1]+dp[i-2];
+//          }
+//          return dp[n];
+//         }
+ 
     
+    
+        int climbStairs(int n) {
+//    Tabulation
+        int prev2=1;
+        int prev1=1;
+            
+            for(int i=2;i<=n;i++){
+                int curr=prev2+prev1;
+                prev2=prev1;
+                prev1=curr;
+            }
+            
+          return prev1;
+            
+        }
 };
