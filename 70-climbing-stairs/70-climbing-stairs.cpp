5cd47f7f25cc7415e8611ef652008a8545192410
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-08 13:08:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.27%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -16,18 +16,35 @@ public:
 //     }
     
     
-    int climbStairs(int n) {
+//     int climbStairs(int n) {
+    
+//     unordered_map<int,int>dp;
+ 
+//     dp[0]=1;
+//     dp[1]=1;
+        
+//         for(int i=2;i<=n;i++) {
+            
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+      
+//         return dp[n];
+//      }
+    
+        int climbStairs(int n) {
     
     unordered_map<int,int>dp;
- 
-    dp[0]=1;
-    dp[1]=1;
-        
+      
+      int prev=1;
+      int prev1=1;
+            
         for(int i=2;i<=n;i++) {
             
-            dp[i]=dp[i-1]+dp[i-2];
+          int  curr=prev+prev1;
+            prev1=prev;
+            prev=curr;
         }
       
-        return dp[n];
+        return prev;
      }
 };
