5004448b72ec7192ea53504aa3341649e2431d1b
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-12 22:25:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.59%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     
     int f(int n,unordered_map<int,int> &dp){
-       if(n==0) return 1;
-       if(n==1) return 1; 
-        
-     
+      
+       dp[0]=1;
+       dp[1]=1; 
        
-       if(dp.find(n) != dp.end()){
-           return dp[n];
+       for(int i=2;i<=n;i++){
+           dp[i]=dp[i-1]+dp[i-2];
        }
        
-       return dp[n]=f(n-1,dp)+f(n-2,dp);        
+       return dp[n];       
     }
     
    int climbStairs(int n) {
