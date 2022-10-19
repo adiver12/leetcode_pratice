1aee122b47fe840719b66eab6a7bd3092ece3de1
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-19 17:01:16 +0530

    Time: 512 ms (46.25%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
 
-SELECT firstName,lastName, city, state 
+# SELECT firstName,lastName, city, state 
+# FROM Person
+# LEFT JOIN Address
+# ON Person.PersonId = Address.PersonId
+# ORDER BY firstName;
+
+SELECT firstName, lastName, city, state
 FROM Person
 LEFT JOIN Address
-ON Person.PersonId = Address.PersonId
-ORDER BY firstName;
-
+ON Person.personId=Address.personId;
