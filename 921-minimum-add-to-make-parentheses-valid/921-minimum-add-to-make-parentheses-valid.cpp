2f5334cc09f2c60d6026fb95a08723a900e1ff33
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2022-10-22 10:28:40 +0530

    Time: 3 ms (57.34%), Space: 6.2 MB (84.53%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -3,13 +3,16 @@ public:
     int minAddToMakeValid(string S) {
          
        int need_left = 0, need_right = 0;
-        for (char c : S)
+        
+        for (char c : S) {
             if (c == '(')
                 need_right++;
             else if (need_right > 0)
                 need_right--;
             else
                 need_left++;
+        }
+        
         return need_left + need_right;
   
     }
