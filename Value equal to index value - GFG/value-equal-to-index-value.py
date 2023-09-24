7f98fbd9fbf2e83e72afbdfeb627d13cf95aa36f
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2023-09-24 12:57:36 +0530

    Added solution - LeetHub

--- Value equal to index value - GFG/value-equal-to-index-value.py
+++ Value equal to index value - GFG/value-equal-to-index-value.py
@@ -4,18 +4,13 @@ class Solution:
 	def valueEqualToIndex(self,arr, n):
 		# code here
 		
-# 		ans = []
-# 		for i in range(n):
-# 		    if (arr[i] == i+1):
-# 		        ans.append(i+1)
-# 		return ans
+		l = []
 		
-		ans = []
 		for i in range(n):
 		    if (arr[i] == i+1):
-		        ans.append(i+1)
-		return ans
-		
+		        l.append(i+1)
+		return l
+
 
 		 
 		
