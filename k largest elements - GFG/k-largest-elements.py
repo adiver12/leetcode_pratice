79d42d7322c3470f8fc2734cf324d1b2bf69ca56
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2023-09-24 13:40:26 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+    
+
+import heapq
+class Solution:
+
+    def kLargest(self,arr, n, k):
+        # code here
+
+        q = []
+        
+        for i in arr:
+            heapq.heappush(q,i)
+            if len(q) > k:
+                heapq.heappop(q)
+        
+        q.sort(reverse = True)
+        return q
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.kLargest(arr, n, k)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
