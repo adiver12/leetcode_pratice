f348bd16f77375e6516b0884f653261ce3ac90fb
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2023-09-29 16:07:06 +0530

    Added solution - LeetHub

---
+++ Reverse a Stack - GFG/reverse-a-stack.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+from typing import List
+
+class Solution:
+    def reverse(self,St): 
+        #code here
+        i=0
+        j=len(St)-1
+        while i<j:
+            temp=St[i]
+            St[i]=St[j]
+            St[j]=temp
+            i+=1
+            j-=1
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+ 
+for _ in range(int(input())):
+    N = int(input())
+    St = list(map(int, input().split()))
+    ob = Solution()
+    ob.reverse(St)
+    print(*St)
+# } Driver Code Ends
