f4efb34d90d98068ca7d480632d564c8be7f6fc9
Aditya Verma <109779311+adiver12@users.noreply.github.com>
2023-10-12 11:52:37 +0530

    Added solution - LeetHub

--- Power of 2 - GFG/power-of-2.py
+++ Power of 2 - GFG/power-of-2.py
@@ -6,9 +6,18 @@ class Solution:
     def isPowerofTwo(self,n):
         
         ##Your code here
-        if n ==0:
+
+        # if n==0:
+        #     return False
+        # return (n & n-1) == 0
+        
+        if n==0:
             return False
-        return (n & n-1) == 0      
+        return (n &(n-1)) == 0
+        
+        
+        
+
 
 
 #{ 
